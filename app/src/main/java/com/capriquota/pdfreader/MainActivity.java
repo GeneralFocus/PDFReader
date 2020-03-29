a248259d04167391cff96198880e7c5e0d33fd95
generalfocus <generalfocus1@gmail.com>
2020-03-29 17:55:31 +0100

    PDF file added to assets & Bug Fix

--- app/src/main/java/com/capriquota/pdfreader/MainActivity.java
+++ app/src/main/java/com/capriquota/pdfreader/MainActivity.java
@@ -1,8 +1,9 @@
 package com.capriquota.pdfreader;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.util.FitPolicy;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        PDFView pdfView = findViewById(R.id.pdfReader);
+        pdfView.fromAsset("mobdy.pdf")
+                .enableSwipe(true)
+                .swipeHorizontal(true)
+                .enableDoubletap(true)
+                .defaultPage(0)
+                .enableAnnotationRendering(false)
+                .password(null) // incase your pdf has a password you can set it here
+                .scrollHandle(null)
+                .enableAntialiasing(true)
+                .spacing(0)
+                .pageFitPolicy(FitPolicy.WIDTH)
+                .load();
     }
 }
